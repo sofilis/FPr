24992939cbdb2dcb457e4f6dd47ab49e19d95422
sophie <coni18042006@gmail.com>
2022-11-30 12:52:20 +0300

    addsheme

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите массив строк через пробел");
 string stroka = Console.ReadLine()!;
-string[] M = stroka.Split(' ');
-var result = new string[M.Length];
-var realSize = 0;
+string[] main = stroka.Split(' ');
+var res = new string[M.Length];
+var realSize = 0;()
 foreach(var value in M) {
   if (value.Length <= 3) {
     result[realSize] = value;
